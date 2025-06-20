4769b8998e9a5a73ad2b4f8832d63362dd76910f
Nitin Reddy Kamsani <kamsaninitinreddy@gmail.com>
2025-06-20 12:43:55 +0530

    Layout

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+        <h1 className='text-3xl font-bold'>Youtube</h1>
+        <h1>Head</h1>
+    </div>
+  )
+}
+
+export default App
